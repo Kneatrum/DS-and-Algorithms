62957200159cbf329bdcc6fb8a96bb9bcf06ca75
Kneatrum <martin.mwiti.n@gmail.com>
2023-12-25 03:19:24 +0300

    Reorganisation - Renamed the LinkedList class to SinglyLinkedList. - Deleted the function calls (That's now being done inside the workspace.py file. My little playground ;-)

--- python/linked lists/singly_linked_list.py
+++ python/linked lists/singly_linked_list.py
@@ -4,7 +4,7 @@ class Node:
         self.value = value
         self.next = None
 
-class LinkedList:
+class SinglyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
@@ -276,65 +276,6 @@ class LinkedList:
             
             current = current.next
 
-
-
-
-
-
-
-
-    
-
-
-
-
-new_linked_list = LinkedList()
-
-new_linked_list.append(10)
-new_linked_list.append(20)
-new_linked_list.append(30)
-new_linked_list.append(40)
-new_linked_list.append(50)
-
-print(new_linked_list)
-print("Length of linked list: " + str(new_linked_list.length))
-
-new_linked_list.prepend(5)
-print(new_linked_list)
-print("Length of linked list: " + str(new_linked_list.length))
-
-new_linked_list.insert(2, 15)
-print(new_linked_list)
-print("Length of linked list: " + str(new_linked_list.length))
-
-new_linked_list.traverse()
-
-if (new_linked_list.search(2)):
-    print("Found")
-else:
-    print("Not found")
-
-result = new_linked_list.get(0)
-if (result):
-    print(result)
-else:
-    print("No element found in that index")
-
-new_linked_list.update(2, 5)
-print(new_linked_list)
-
-new_linked_list.pop_first()
-print(new_linked_list)
-
-
-new_linked_list.delete(-1)
-print(new_linked_list)
-
-# new_linked_list.delete_all()
-# print(new_linked_list)
-
-new_linked_list.reverse()
-print(new_linked_list)
 
 
 
