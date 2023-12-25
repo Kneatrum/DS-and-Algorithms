0de93683801ad258e66dc6bfd2d0690c27d5f2fa
Kneatrum <martin.mwiti.n@gmail.com>
2023-12-25 03:21:57 +0300

    My Playground ;-) - Created this file and transferred all the function calls into it. - Tests will be done here after importing my python modules.

---
+++ python/linked lists/workspace.py
@@ -0,0 +1,59 @@
+from singly_linked_list import *
+from circular_singly_linked_list import *
+
+
+
+
+
+
+
+
+
+
+new_linked_list = SinglyLinkedList()
+
+new_linked_list.append(10)
+new_linked_list.append(20)
+new_linked_list.append(30)
+new_linked_list.append(40)
+new_linked_list.append(50)
+
+print(new_linked_list)
+print("Length of linked list: " + str(new_linked_list.length))
+
+new_linked_list.prepend(5)
+print(new_linked_list)
+print("Length of linked list: " + str(new_linked_list.length))
+
+new_linked_list.insert(2, 15)
+print(new_linked_list)
+print("Length of linked list: " + str(new_linked_list.length))
+
+new_linked_list.traverse()
+
+if (new_linked_list.search(2)):
+    print("Found")
+else:
+    print("Not found")
+
+result = new_linked_list.get(0)
+if (result):
+    print(result)
+else:
+    print("No element found in that index")
+
+new_linked_list.update(2, 5)
+print(new_linked_list)
+
+new_linked_list.pop_first()
+print(new_linked_list)
+
+
+new_linked_list.delete(-1)
+print(new_linked_list)
+
+# new_linked_list.delete_all()
+# print(new_linked_list)
+
+new_linked_list.reverse()
+print(new_linked_list)
