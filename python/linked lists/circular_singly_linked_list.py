5db13fb41e9713c446c8488d58c0bea218c851bc
Kneatrum <martin.mwiti.n@gmail.com>
2023-12-26 22:06:19 +0300

    Circular singly linked list implementation

---
+++ python/linked lists/circular_singly_linked_list.py
@@ -0,0 +1,25 @@
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class CircularSinglyLinkedList:
+
+    # Empty circular linked list
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    '''
+    # Circular linked list with one node.
+    def __init__(self, value):
+        new_node = Node(value)
+        new_node.next = new_node
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    '''
+
+
+c_linked_list = CircularSinglyLinkedList()
