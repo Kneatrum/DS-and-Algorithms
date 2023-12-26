1629f05afdf30902630c304bdfbd03445ee181f8
Kneatrum <martin.mwiti.n@gmail.com>
2023-12-26 22:37:29 +0300

    Append method.

--- python/linked lists/circular_singly_linked_list.py
+++ python/linked lists/circular_singly_linked_list.py
@@ -20,6 +20,20 @@ class CircularSinglyLinkedList:
         self.tail = new_node
         self.length = 1
     '''
-    
+
+    def append(self, value):
+        new_node = Node(value)
+        # If the linked list is empty, create a new node and let the tail point to itself.
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+            self.tail.next = new_node
+        # If the linked list has one or more nodes, add the newo one and let the tail point to the head.
+        else:
+            self.tail.next = new_node
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        
 
 
