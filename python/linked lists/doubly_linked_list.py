803bd48bcde70ce477391ec6366a4ca163050798
Kneatrum <martin.mwiti.n@gmail.com>
2023-12-29 22:14:42 +0300

    The rest of Doubly Linked List Methods. - Pop first - Pop (last) - Get - Set - Delete - Search - Insert

--- python/linked lists/doubly_linked_list.py
+++ python/linked lists/doubly_linked_list.py
@@ -19,7 +19,7 @@ class DoublyLinkedList:
         temp_node = self.head
         result = ""
         # Add the first null node to the string.
-        if self.length > 0:
+        if self.length > 1:
             result += str(self.head.prev) + " <--> "
 
         while temp_node:
@@ -34,8 +34,7 @@ class DoublyLinkedList:
         return result
 
         
-
-
+    # Adding a node to the doubly linked list
     def append(self, value):
         new_node = Node(value)
         if self.head == None:
@@ -59,6 +58,7 @@ class DoublyLinkedList:
             new_node.next = self.head
             self.head.prev = new_node
             self.head = new_node
+            self.length += 1
 
     
     # Traversing the doubly linked list forwards
@@ -81,3 +81,126 @@ class DoublyLinkedList:
             if temp_node == None:
                 print()
                 return
+            
+    
+    # Removing the first node from the list
+    def pop_first(self):
+        # If the linked list is empty, return none.
+        if self.head == None:
+            return None
+        # If the length of the list is 1, delete it from the list
+        to_pop = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return to_pop
+        # Else, set the second node as the head of the list and delete the first node.
+        else:
+            self.head = to_pop.next
+            self.head.prev = None
+            to_pop.next = None
+            self.length -= 1
+            return to_pop
+    
+
+    # Removing the last node from the list
+    def pop_last(self):
+        # If the list is empty, return None
+        if self.length == 0:
+            return False
+        # If the length of the list is 1, delete it from the list
+        to_pop = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return to_pop
+        # Else, set the second last node as the tail and delete the last node.
+        else:
+            self.tail = to_pop.prev
+            self.tail.next = None
+            to_pop.prev = None
+            to_pop.next = None
+            self.length -= 1
+            return to_pop
+    
+
+    # Getting a node from a specific position in the doubly linked list.
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if self.head is None or self.length == 0:
+            return None
+        # If the index  is less than half of the length, start from the beginning.
+        if index < self.length // 2:
+            temp_node = self.head
+            for _ in range(index):
+                temp_node = temp_node.next
+        # Else, start from the tail if the index is more than half of the length of the linked list.
+        else:
+            temp_node = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp_node = temp_node.prev
+        return temp_node
+    
+
+    # Setting the value of a node from a specific position in the doubly linked list.
+    def set(self, index, value):
+        node =  self.get(index)
+        if node is not None:
+            node.value = value
+            return True
+        return False
+
+
+    # Deleting a node from a specific position in the doubly linked list.
+    def delete(self, index):
+        if index < 0 or index >= self.length:
+            return
+        if index == 0:
+            return self.pop_first()
+        elif index == self.length - 1:
+            return self.pop_last()
+        else:
+            to_delete = self.get(index)
+            to_delete.prev.next = to_delete.next
+            to_delete.next.prev = to_delete.prev
+            to_delete.next = None
+            to_delete.prev = None
+            self.length -= 1
+            return to_delete
+
+
+    # Searching a node in the doubly linked list
+    def search(self, target):
+        temp_node = self.head
+        while temp_node is not None:
+            if temp_node.value == target:
+                return True
+            temp_node = temp_node.next
+        return False
+    
+
+    # Insert method
+    def insert(self, index, value):
+        if index < 0 or index >= self.length:
+            print("Index out of range")
+            return 
+        elif index == 0:
+            self.prepend(value)
+            return
+        else:
+            new_node = Node(value)
+            node =  self.get(index - 1)
+            new_node.next = node.next
+            new_node.prev = node
+            node.next.prev = new_node
+            node.next = new_node
+            self.length += 1
+        
+
+        
+
+
+
