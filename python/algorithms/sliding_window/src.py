870b2c04e0cd13cf8ce0e0e651eca109da777470
Kneatrum <martin.mwiti.n@gmail.com>
2024-03-08 22:20:27 +0300

    Finding the best time to buy and sell stock for maximum profit

--- python/algorithms/sliding_window/src.py
+++ python/algorithms/sliding_window/src.py
@@ -12,7 +12,26 @@ def sliding_window(data, n):
     for i in range(len(data) - n):
         total -= data[i] # Remove the first element from the window
         total += data[i + n] # Add the next element to the window
-        max_total = max(max_total,total) # Assign the maximum total to the max total value
+        max_total = max(max_total,total) # Assign the maximum total to the max total value 
     return max_total
 
 print(sliding_window(data_list, window_size))
+
+# Find the best time to buy and sell stock for maximum profit in the price list below.
+
+prices = [2,5,33,9,20,63,49,6,13,27,99,71,1,37,22]
+ 
+def two_pointers(data):
+    left, right = 0, 1
+    max_profit = 0
+
+    while right < len(data):
+        if data[right] > data[left]:
+            profit = data[right] - data[left]
+            max_profit = max(max_profit, profit)
+        else:
+            left = right
+        right += 1
+    return max_profit
+
+print(two_pointers(prices))
